ff72f6431917d8a6ecc129499e313cc647792e39
Balkrishna6420 <your-bkrishnajha494@gmail.com>
2025-02-28 01:29:29 +0530

    Multiple Routers

--- src/app.js
+++ src/app.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const app = express();
-app.get("/user", (req, res) => {
-  res.send({ firstName: "BAlkrishna", lastNAme: "Jha" });
-});
-app.post("/user", (req, res) => {
-  res.send("Data Saved in database successfully");
-});
-app.delete("/user", (req, res) => {
-  res.send("Deleted Successfully");
-});
-app.use("/test", (req, res) => {
-  res.send("Testing in my world");
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2!!");
+    // res.send("2nd Response!!");
+    next();
+  },
+
+  (req, res, next) => {
+    console.log("Handling the route user 3!!");
+    // res.send("3rd Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 4!!");
+    // res.send("4th Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 5!!");
+    res.send("5th Response!!");
+  }
+);
 app.listen(7777, () => {
   console.log("Server is successfully connected to port 7777");
 });
